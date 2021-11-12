0795cb950e04b977514dc1bf5b6add4ea2badaa6
jinseonghee <wlstjdgml0610@gmail.com>
2021-11-12 23:46:21 +0900

    feat: 기수 변환

---
+++ 자료구조와_함께_배우는_알고리즘_입문/src/com/company/chap02/CardConvRev.java
@@ -0,0 +1,15 @@
+package com.company.chap02;
+
+public class CardConvRev {
+
+    static int cardConvR(int x, int r, char[] d){
+        int digits = 0;
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        do {
+            d[digits++] = dchar.charAt(x % r);
+            x /= r;
+        } while (x != 0);
+        return digits;
+    }
+}
