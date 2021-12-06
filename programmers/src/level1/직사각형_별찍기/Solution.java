fcc8e5f0dce1e0d040db98448679be1dc15b2916
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-06 19:03:21 +0900

    refector: 직사각형_별찍기

--- programmers/src/level1/직사각형_별찍기/Solution.java
+++ programmers/src/level1/직사각형_별찍기/Solution.java
@@ -9,11 +9,11 @@ public class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for (int i = 0; i < b; i++) {
-            for (int j = 1; j < a; j++) {
+        for (int i = 1; i <= b; i++) {
+            for (int j = 1; j <= a; j++) {
                 System.out.print("*");
             }
-            System.out.println("*");
+            System.out.println();
         }
     }
 }
