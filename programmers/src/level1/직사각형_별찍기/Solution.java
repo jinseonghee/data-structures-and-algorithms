b7de7c09de8b341dbffe96f66764b8ec6e128fdb
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-06 18:51:09 +0900

    feat: 직사각형 별 찍기

---
+++ programmers/src/level1/직사각형_별찍기/Solution.java
@@ -0,0 +1,21 @@
+package level1.직사각형_별찍기;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 1; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
+
+
