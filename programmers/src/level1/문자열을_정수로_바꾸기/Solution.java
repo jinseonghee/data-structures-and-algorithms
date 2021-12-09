a0d7dd529aaea881caa81598b9735d16fcfdd084
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-09 18:55:37 +0900

    feat: 문자열을 정수로 바꾸기

---
+++ programmers/src/level1/문자열을_정수로_바꾸기/Solution.java
@@ -0,0 +1,11 @@
+package level1.문자열을_정수로_바꾸기;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
