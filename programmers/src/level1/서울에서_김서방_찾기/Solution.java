bd6ccac66a073835b8e461263879fde626d2f6b3
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-14 23:30:55 +0900

    feat: 서울에서 김서방 찾기

---
+++ programmers/src/level1/서울에서_김서방_찾기/Solution.java
@@ -0,0 +1,20 @@
+package level1.서울에서_김서방_찾기;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    public static String solution(String[] seoul) {
+
+        Arrays.asList(seoul);
+        int result = Arrays.asList(seoul).indexOf("kim");
+        String answer = "김서방은 " + result + "에 있다.";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(new String[]{"jane", "kim" ,"tttt"}));
+    }
+}
