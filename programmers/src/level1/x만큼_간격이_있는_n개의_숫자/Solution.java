73fd19e26552d2d78f0ea36ea4018212a03e4b66
jinseonghee <wlstjdgml0610@gmail.com>
2021-12-21 21:01:16 +0900

    feat: x만큼 간격이 있는 n개의 숫자

--- programmers/src/level1/x만큼_간격이_있는_n개의_숫자/Solution.java
+++ programmers/src/level1/x만큼_간격이_있는_n개의_숫자/Solution.java
@@ -1,2 +1,15 @@
-package level1.x만큼_간격이_있는_n개의_숫자;public class Solution {
+package level1.x만큼_간격이_있는_n개의_숫자;
+
+public class Solution {
+
+	public long[] solution(int x, int n) {
+
+		long[] answer = new long[n];
+		long x1 = x;
+
+		for (int i = 0; i < n; i++) {
+			answer[i] = x1 * (i + 1);
+		}
+		return answer;
+	}
 }
