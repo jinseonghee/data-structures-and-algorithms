59d632588d4a2f776f487dd8f15247377d3cea71
jinseonghee <wlstjdgml0610@gmail.com>
2022-01-05 17:56:50 +0900

    feat: practice question 3-6

---
+++ 자료구조와_함께_배우는_알고리즘_입문/src/com/company/chap03/practice/Question6.java
@@ -0,0 +1,39 @@
+package com.company.chap03.practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("요솟수 : ");
+
+		int num = scan.nextInt();
+		int[] x = new int[num];
+
+		System.out.println("오름차순으로 입력해 주세요.");
+
+		System.out.print("x[0]: ");
+		x[0] = scan.nextInt();
+
+		for (int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "] : ");
+				x[i] = scan.nextInt();
+			} while (x[i] < x[i - 1]);
+		}
+
+		System.out.print("검색할 값 : ");
+		int ky = scan.nextInt();
+
+		int idx = Arrays.binarySearch(x, ky);
+
+		if (idx < 0)
+			System.out.println("삽입 포인트는 " + idx + "입니다.");
+		else
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다. ");
+	}
+}
