b6e7b729d6d80e28f103efef845e550742d10363
jinseonghee <wlstjdgml0610@gmail.com>
2022-01-12 17:04:26 +0900

    feat: 실습 6-4

---
+++ 자료구조와_함께_배우는_알고리즘_입문/src/com/company/chap06/SelectionSort.java
@@ -0,0 +1,45 @@
+package com.company.chap06;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+
+	//단순 선택 정렬
+	static void SelectionSort(int[] a, int n) {
+
+		for(int i = 0; i < n - 1; i++) {
+			int min = i; // 아직 정렬되지 않은 부분에서 가장 작은 요소의 인덱스 기록
+			for(int j = i + 1; j < n; j++)
+				if (a[j] < a[min])
+					min = j;
+				swap(a, i, min); // 아직 정렬되지 않은 부분의 첫 요소와 가장 작은 요소를 교환
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("버블 정렬(버전1)");
+		System.out.print("요솟수 : ");
+		int nx = scan.nextInt();
+		int[] x = new int[nx];
+
+		for(int i = 0; i < nx; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = scan.nextInt();
+		}
+
+		SelectionSort(x, nx); //배열 x를 버블 정렬 합니다.
+
+		System.out.println("오름차순으로 정렬했습니다.");
+		for (int i = 0; i < nx; i++)
+			System.out.println("x[" + i + "]=" + x[i]);
+	}
+}
