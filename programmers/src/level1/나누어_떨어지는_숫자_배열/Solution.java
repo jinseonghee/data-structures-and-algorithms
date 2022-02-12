4cc3fdd9918ebfe0568fe5b381c36c4a1dee919b
jinseonghee <wlstjdgml0610@gmail.com>
2022-02-12 23:12:13 +0900

    refector: 나누어 떨어지는 숫자 배열

--- programmers/src/level1/나누어_떨어지는_숫자_배열/Solution.java
+++ programmers/src/level1/나누어_떨어지는_숫자_배열/Solution.java
@@ -1,29 +1,25 @@
 package level1.나누어_떨어지는_숫자_배열;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class Solution {
 
-	public int[] solution(int[] arr, int divisor) {
-		int[] answer = {};
-		return answer;
+	public static int[] solution(int[] arr, int divisor) {
+
+		int[] resultIntArray = Arrays.stream(arr)
+			.filter(it -> it % divisor == 0)
+			.sorted()
+			.toArray();
+
+		if (resultIntArray.length == 0) {
+			return new int[] {-1};
+		}
+
+		return resultIntArray;
 	}
 
 	public static void main(String[] args) {
-
-		int[] arr = {5, 9, 7, 10};
-		Arrays.sort(arr);
-		int divisor = 5;
-		int answer = 0;
-		int[] digits = {};
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] % divisor == 0) {
-				answer = arr[i];
-				digits = Stream.of(String.valueOf(answer).split(",")).mapToInt(Integer::parseInt).toArray();
-				System.out.print(Arrays.toString(digits));
-			}
-		}
+		Arrays.stream(solution(new int[] {2, 36, 1, 3}, 1)).forEach(System.out::println);
 	}
 }
+
